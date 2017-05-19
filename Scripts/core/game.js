7836cccb91afc8c0e601df7d71ad61bf4bd96923
jmontene <josmontenegro2012@gmail.com>
2017-05-19 11:38:21 -0400

    updated game.js

---
+++ Scripts/core/game.js
@@ -0,0 +1,16 @@
+//IIFE -- Immediately Invoked Function Expression
+(function() {
+    function Start(){
+        console.log("Game Started");
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+
+    }
+
+    window.onload = Start;
+})();
